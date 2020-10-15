e2029e424b754c966ee49ba6b51d076b4f83c723
LaustFaerch <laust.faerch@gmail.com>
2020-10-15 12:40:42 +0200

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wapor-et-look',
+    description='Computing Evapotranspiration of large areas using remote sensing data',
+    author='DHI GRAS',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
