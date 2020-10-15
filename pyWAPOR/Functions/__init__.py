451a8682d96b30bef76880b1276111761eab5a4e
LaustFaerch <laust.faerch@gmail.com>
2020-10-15 12:56:56 +0200

    Added new functions to __init__.py

--- pyWAPOR/Functions/__init__.py
+++ pyWAPOR/Functions/__init__.py
@@ -4,8 +4,10 @@ Authors: Tim Hessels
 Module: WAPOR/Functions
 """
 
-from pyWAPOR.Functions import WaitbarConsole, Processing_Functions, LandCover_Converter, Swets_Filter
+from pyWAPOR.Functions import WaitbarConsole, Processing_Functions, LandCover_Converter, Swets_Filter, nspi, \
+    landsat_modis_fusion
 
-__all__ = ['WaitbarConsole', 'Processing_Functions', 'LandCover_Converter', 'Swets_Filter']
+__all__ = ['WaitbarConsole', 'Processing_Functions', 'LandCover_Converter', 'Swets_Filter', 'nspi',
+           'landsat_modis_fusion']
 
 __version__ = '0.1'
