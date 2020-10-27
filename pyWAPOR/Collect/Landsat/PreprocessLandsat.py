52812142f45d92d6bd07ccb3e8bf2ae9d7062adf
LaustFaerch <laust.faerch@gmail.com>
2020-10-27 16:24:32 +0100

    solved issue with L7 and L8 having different band order

--- pyWAPOR/Collect/Landsat/PreprocessLandsat.py
+++ pyWAPOR/Collect/Landsat/PreprocessLandsat.py
@@ -31,15 +31,17 @@ def PreprocessLandsat(landsat_dir):
     L7_dirs = [directory for directory in list((landsat_dir / Path('L7')).glob('*')) if os.path.isdir(directory)]
     L8_dirs = [directory for directory in list((landsat_dir / Path('L8')).glob('*')) if os.path.isdir(directory)]
 
+    filename_list = []
+    bandnames_list = []
+
     print('Merging Landsat bands...')
-    for directory in tqdm(L7_dirs + L8_dirs):
-        band_names = _merge_and_save_landsat(directory, delete_input=True)
+    for directory in tqdm(L7_dirs+L8_dirs):
+        filename, band_names = _merge_and_save_landsat(directory, delete_input=False)
+        filename_list.append(filename)
+        bandnames_list.append(band_names)
 
-    L7_tifs = list((landsat_dir / Path('L7')).glob('*T1.tif')) + list((landsat_dir / Path('L7')).glob('*T2.tif'))
-    L8_tifs = list((landsat_dir / Path('L8')).glob('*T1.tif')) + list((landsat_dir / Path('L8')).glob('*T2.tif'))
-
-    # apply nspi gap-filling and save files (slow!)
-    _apply_nspi(L7_tifs, L8_tifs, band_names)
+    # apply nspi gap-filling on the landsat-7 data (slow!)
+    _apply_nspi(landsat_dir, filename_list, band_names)
 
     # calculate NDVI/Albedo
 
@@ -53,69 +55,77 @@ def PreprocessLandsat(landsat_dir):
         # Calculate dekadal NDVI as mean of the dekade
 
 
-def _apply_nspi(L7_tifs, L8_tifs, band_names):
+def _apply_nspi(landsat_dir, filename_list, bandnames_list):
+    L7_idx = [i for i, file in enumerate(filename_list) if str(file.split('_')[0]) == 'LE07']
+    L8_idx = [i for i, file in enumerate(filename_list) if str(file.split('_')[0]) == 'LC08']
+
+    L7_tifs = [filename_list[idx] + '.tif' for idx in L7_idx]
+    L8_tifs = [filename_list[idx] + '.tif' for idx in L8_idx]
+
+    L7_bandnames = [bandnames_list[idx] for idx in L7_idx]
+    L8_bandnames = [bandnames_list[idx] for idx in L8_idx]
+
     L7_dates = [datetime.strptime(str(f).split('_')[3], '%Y%m%d') for f in L7_tifs]
     L8_dates = [datetime.strptime(str(f).split('_')[3], '%Y%m%d') for f in L8_tifs]
 
     print('Fill gaps (NSPI)...')
-    for i, L7_file in enumerate(tqdm(L7_tifs)):
+    for L7_idx, L7_file in enumerate(tqdm(L7_tifs)):
 
-        L7_date = L7_dates[i]
-        date_diff = [np.abs(L7_date-L8_date) for L8_date in L8_dates]
-        L8_file = L8_tifs[np.argmin(date_diff)]
+        L7_date = L7_dates[L7_idx]
+        date_diff = [np.abs(L7_date - L8_date) for L8_date in L8_dates]
+        L8_idx = np.argmin(date_diff)
+        L8_file = L8_tifs[L8_idx]
 
         # open target image
-        with rio.open(str(L7_file)) as master_src:
+        with rio.open(str(landsat_dir / Path('L7') / Path(L7_file))) as master_src:
             meta = master_src.profile
-            target_image = master_src.read()
+            L7_image = master_src.read()
 
         master_dict = {'transform': master_src.transform, 'height': master_src.height, 'width': master_src.width,
                        'crs': master_src.crs}
 
         # open input image
-        with rio.open(str(L8_file)) as slave_src:
+        with rio.open(str(landsat_dir / Path('L8') / Path(L8_file))) as slave_src:
             with WarpedVRT(slave_src, **master_dict) as vrt:
-                input_image = vrt.read()
+                L8_image = vrt.read()
 
         bands = ['sr_band1', 'sr_band2', 'sr_band3', 'sr_band4', 'sr_band5', 'sr_band7', ]
 
-        # if ~len(list(set(bands) & set(band_names)))==len(bands):
-        #     print('bands not found') # TODO: add this check
-
-        band_idx = [band_names.index(band) for band in bands]
+        L7_band_idx = [L7_bandnames[L7_idx].index(band) for band in bands]
+        L8_band_idx = [L8_bandnames[L8_idx].index(band) for band in bands]
 
         # reshape images for correct NSPI format
-        input_image_reshaped = np.transpose(input_image[band_idx, ...], axes=[1, 2, 0])
-        target_image_reshaped = np.transpose(target_image[band_idx, ...], axes=[1, 2, 0])
+        L7_image_reshaped = np.transpose(L7_image[L7_band_idx, ...], axes=[1, 2, 0])
+        L8_image_reshaped = np.transpose(L8_image[L8_band_idx, ...], axes=[1, 2, 0])
 
         # add nans instead of nodata
-        input_image_reshaped = np.where(input_image_reshaped == -9999, np.nan, input_image_reshaped)
-        target_image_reshaped = np.where(target_image_reshaped == -9999, np.nan, target_image_reshaped)
+        L7_image_reshaped = np.where(L7_image_reshaped == -9999, np.nan, L7_image_reshaped)
+        L8_image_reshaped = np.where(L8_image_reshaped == -9999, np.nan, L8_image_reshaped)
 
         # calculate the missing pixels mask
-        missing_pixels_mask = np.isnan(target_image_reshaped[..., 0]) & ~np.isnan(input_image_reshaped[..., 0])
+        missing_pixels_mask = np.isnan(L7_image_reshaped[..., 0]) & ~np.isnan(L8_image_reshaped[..., 0])
 
         # get the bitmask
-        target_pixel_qa = target_image[0, ...]
-        target_pixel_qa = np.where(target_pixel_qa == -9999, 0, target_pixel_qa)
+        L7_pixel_qa = L7_image[L7_bandnames[L7_idx].index('pixel_qa'), ...]
+        L7_pixel_qa = np.where(L7_pixel_qa == -9999, 0, L7_pixel_qa)
 
         # calculate cloud mask
-        target_pixel_cloudmask = _landsat_cloudmask(target_pixel_qa)
+        L7_pixel_cloudmask = _landsat_cloudmask(L7_pixel_qa)
 
         # mask clouds
         for n in range(0, len(bands)):
-            target_image_reshaped[..., n] = np.where(target_pixel_cloudmask, np.nan, target_image_reshaped[..., n])
+            L7_image_reshaped[..., n] = np.where(L7_pixel_cloudmask, np.nan, L7_image_reshaped[..., n])
 
-        out_image_reshaped = nspi(input_image_reshaped, target_image_reshaped, missing_pixels_mask,
+        out_image_reshaped = nspi(L8_image_reshaped, L7_image_reshaped, missing_pixels_mask,
                                   num_classes=5, required_pixels=20, max_window_size=15)
 
-        out_image = target_image.copy()
+        out_image = L7_image.copy()
 
-        out_image[3:9, ...] = np.transpose(out_image_reshaped, axes=[2, 0, 1])
+        out_image[L7_band_idx, ...] = np.transpose(out_image_reshaped, axes=[2, 0, 1])
 
         out_image = np.where(np.isnan(out_image), -9999, out_image).astype(np.int16)
 
-        output_filename = L7_file.parent / Path(L7_file.stem + '_gap-filled.tif')
+        output_filename = str(Path(L7_file).parent / Path(L7_file).stem) + '_gap-filled.tif'
 
         with rio.open(output_filename, 'w', **meta) as dst:
             dst.write(out_image)
@@ -170,7 +180,7 @@ def _merge_and_save_landsat(directory, delete_input=False):
     if delete_input:
         shutil.rmtree(directory)
 
-    return band_names
+    return output_filename.stem, band_names
 
 
 # unpack and saves *.tar.gz files
